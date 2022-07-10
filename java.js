06da7d31d3481342e37fa0159e665069f724dae8
lucphyl <lucphyl@gmail.com>
2022-07-10 18:09:24 -0300

    botão implementado e função liberar botão implementada

--- java.js
+++ java.js
@@ -1,13 +1,17 @@
 // nome do usuario
 let dono = prompt ("por favor digite seu nome");
-objeto.owner= dono;
+
 
 //variaveis simples
 let esc1 = 0;
 let esc2 = 0;
 let esc3 = 0;
-let pedido = esc1+esc2+esc3;
+let pedido = 0;
 let estampaURL = document.querySelector(".imagemURl");
+
+// get e post axios
+const pedidosProntos = axios.get ('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
+pedidosProntos.then(UltimosPedidos);
 
 // objeto para postar
 let objeto = {
@@ -18,7 +22,8 @@ image: "",
 owner: "",
 author: "",
 }
-
+objeto.author= dono;
+objeto.owner= dono;
 //escolhas do usuario
 function escolhaModelo (elemento,modelo) {
     const selecionado = document.querySelector(".selecionado");
@@ -33,6 +38,7 @@ function escolhaModelo (elemento,modelo) {
     elemento.classList.add("selecionado");
     esc1 = 1;
     objeto.model= modelo;
+    liberarBotao ();
 }
 
 function escolhaGola (elemento,gola) {
@@ -48,6 +54,7 @@ function escolhaGola (elemento,gola) {
     elemento.classList.add("selecionado1");
     esc2 =1;
     objeto.neck= gola;
+    liberarBotao ();
 }
 
 function escolhaTecido (elemento,tecido) {
@@ -63,14 +70,40 @@ function escolhaTecido (elemento,tecido) {
     elemento.classList.add("selecionado2");
     esc3 =1;
     objeto.material= tecido;
+    liberarBotao ();
 }
-
+liberarBotao ();
 //liberar botão
 function liberarBotao (){
-    
     const livre = document.querySelector(".botaoFechado");
-    if ( pedido===3 && estampaURL.value == true){
+    pedido = esc1 + esc2 +esc3;
+    if (pedido ==3 && objeto.image != ""){
         livre.classList.remove("botaoFechado");
-        livre.classList.add("liberado")
+        livre.classList.add("liberado");
     }
 }
+
+function encomendar (){
+    const encomenda = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', objeto);
+    encomenda.then(tratarSucesso());
+    encomenda.catch(tratarFalha());
+}
+
+function tratarSucesso(resposta) {
+	console.log(resposta); 
+}
+
+function tratarFalha(erro) {
+	console.log(erro);
+}
+
+function enviar(){
+    objeto.image = estampaURL.value;
+    console.table(objeto);
+    liberarBotao ();
+}
+
+function UltimosPedidos(){
+    alert ("servidor liberado");
+}
+
